4b66711630abe3d7c60f8118122e0fc5749e8a67
Jason <740675@aacps.org>
2020-10-30 14:36:11 +0000

    I just completed 006 multiple rollover

--- index.java
+++ index.java
@@ -1,18 +1,37 @@
 void setup() {
 	size(400, 400);
+  
 }
 
 void draw() {
 	background(255);
-	stroke(0);
-
+	stroke(0); 
+  //Fill a background color
+ background(240)
   //start creating the lines
-
-
-  //Fill a black color
-  
-
+line(200,0,200,400);
+line(0,200,400,200);
+ 
   //Build your conditional statement 
-	
+	if (mouseX < width/2 && mouseY < height/2){
+    fill(0);
+    rect(0,0,200,200);
+  }
+ 
+ if (mouseX < width/2 && mouseY > height/2){
+    fill(0);
+    rect(0,200,200,200);
+  }
 
+  if (mouseX > width/2 && mouseY > height/2){
+    fill(0);
+    rect(200,200,200,200);
+  }
+
+  if (mouseX > width/2 && mouseY < height/2){
+    fill(0);
+    rect(200,0,200,200);
+  }
+
+  
 }
